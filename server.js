3aa47e2393b2e65615938250438437c562b70f10
nitzel <jan.s.92@web.de>
2021-01-24 21:24:36 +0000

    Add player history

--- server.js
+++ server.js
@@ -2,6 +2,69 @@
 
 const express = require("express");
 const path = require("path");
+const sqlite3 = require("sqlite3").verbose();
+const fs = require("fs");
+const { exit } = require("process");
+
+function countPlys(notation) {
+  return notation.split(",").length;
+}
+
+function getLatestDatabase(directory = ".") {
+  const dirContent = fs.readdirSync(path.resolve(directory));
+  const dbFilesStats = dirContent
+    .filter((file) => file.endsWith(".db"))
+    .map((name) => ({ name, stats: fs.statSync(name) }))
+    .filter((file) => file && file.stats.isFile());
+
+  if (dbFilesStats.length === 0) {
+    return null;
+  }
+
+  const latestFile = dbFilesStats.reduce((a, b) => (a.stats.ctimeMs > b.stats.ctimeMs ? a : b));
+  return latestFile.name;
+}
+
+function isGameProper(row) {
+  if (row.result !== "1-0" && row.result !== "0-1") {
+    // Road, flat wins and accepted draws are always valid
+    return true;
+  }
+
+  const plyCount = countPlys(row.notation);
+  const size = parseInt(row.size, 10);
+  // console.log(plyCount, size);
+  return plyCount < size * 2; // Not enough moves to warrant a game
+}
+
+function getGamesForPlayer(db, player) {
+  return new Promise((resolve, reject) => {
+    // todo support multiple player names
+    // eslint-disable-next-line max-len
+    const playerNames = player.split(" ");
+    const placeholder = playerNames.map(() => "?").join(",");
+    // eslint-disable-next-line max-len
+    const query = `SELECT * FROM games WHERE player_white COLLATE NOCASE IN (${placeholder}) or player_black COLLATE NOCASE IN (${placeholder}) ORDER BY id DESC`;
+    db.all(
+      query,
+      [...playerNames, ...playerNames],
+      (err, rows) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(rows.filter(isGameProper));
+      },
+    );
+  });
+}
+
+const databasePath = getLatestDatabase();
+if (!databasePath) exit("No .db file found in current directory");
+const db = new sqlite3.Database(databasePath, sqlite3.OPEN_READONLY, (err) => {
+  if (err) {
+    exit(`Error while loading database from '${databasePath}'`, err.message);
+  }
+});
 
 const hostname = "0.0.0.0";
 const port = 8080;
@@ -19,6 +82,16 @@ app.get("/", (req, res) => {
 app.get("/api/rating", (req, res) => {
   // res.statusCode = 200;
   res.sendFile(absoluteRatingPath);
+});
+app.get("/api/player/:playername", async (req, res) => {
+  const { playername } = req.params;
+  getGamesForPlayer(db, playername).then((games) => {
+    // eslint-disable-next-line no-param-reassign
+    games.forEach((game) => { delete game.notation; }); // save ca 60% data
+    res.send(games);
+  }).catch((error) => {
+    res.send({ error });
+  });
 });
 
 app.listen(port, hostname, () => {
--- www/script.js
+++ www/script.js
@@ -13,7 +13,24 @@ function getElements(selector) {
   return document.querySelectorAll(selector);
 }
 
-let statistics = {};
+function getUTCDateString(dateInMsSince1970) {
+  const date = new Date(dateInMsSince1970);
+  // eslint-disable-next-line max-len
+  return `${date.getUTCFullYear()}-${(date.getUTCMonth() + 1).toString().padStart(2, "0")}-${date.getUTCDate().toString().padStart(2, "0")}`;
+}
+
+function getUTCTimeString(dateInMsSince1970) {
+  const date = new Date(dateInMsSince1970);
+  // eslint-disable-next-line max-len
+  return `${date.getUTCHours().toString().padStart(2, "0")}:${date.getUTCMinutes().toString().padStart(2, "0")}`;
+}
+
+function getUTCDateAndTimeString(dateInMsSince1970) {
+  return `${getUTCDateString(dateInMsSince1970)} ${getUTCTimeString(dateInMsSince1970)}`;
+}
+
+// //////////////////////////////////////////
+// Overall statistics
 
 function createPlayerRow(template, rank, player) {
   const clone = template.content.cloneNode(true);
@@ -21,12 +38,14 @@ function createPlayerRow(template, rank, player) {
   tds[0].textContent = rank;
   // eslint-disable-next-line prefer-destructuring
   const [mainName, ...alternatives] = player.name.split(" ");
-  tds[1].querySelector(".main").textContent = mainName;
+  const mainNameElement = tds[1].querySelector(".main");
+  mainNameElement.textContent = mainName;
+  mainNameElement.href = `/static/player.html?name=${player.name}`;
   tds[1].querySelector(".alternatives").textContent = alternatives;
   tds[2].textContent = player.displayrating;
   const delta = player.displayrating - player.oldrating;
   tds[3].textContent = delta < 0 ? delta : `+${delta}`;
-  if (delta > 0) {
+  if (delta >= 0) {
     tds[3].classList.add("up");
     tds[3].classList.remove("down");
   } else {
@@ -47,23 +66,13 @@ function updatePlayerTable(players) {
   playerRows.forEach((row) => tbody.appendChild(row));
 }
 
-function updateStatistics(newStatistics) {
-  statistics = newStatistics;
-
-  getElement("#lastGame").innerHTML = new Date(statistics.timespan.to).toISOString();
+function updateStatistics(statistics) {
+  getElement("#lastGame").innerHTML = `${getUTCDateAndTimeString(statistics.timespan.to)} (UTC)`;
 
   updatePlayerTable(statistics.players);
 }
 
-function onLoad() {
-  console.log("Loaded");
-  getElement("#js-disabled").remove();
-
-  if (!isBrowserSupported()) {
-    console.error("This browser is not supported");
-    getElement("#browser-not-supported").classList.remove("hidden");
-  }
-
+function loadStatistics() {
   const ratingRequest = window.fetch("/api/rating");
   ratingRequest.then((response) => {
     if (response.status !== 200) {
@@ -76,11 +85,100 @@ function onLoad() {
   });
 }
 
-document.onload = onLoad;
+// //////////////////////////////////////////
+// Player
+
+function updatePlayerPage(data) {
+  function createGameRow(template, game) {
+    const clone = template.content.cloneNode(true);
+    const tds = clone.querySelectorAll("td");
+    const round = (n) => (n ? n.toFixed(0) : n);
+    const prefix = (n) => {
+      if (n == null) return "";
+      return n < 0 ? round(n) : `+${round(n)}`;
+    };
+
+    [
+      getUTCDateAndTimeString(game.date),
+      round(game.white_elo),
+      prefix(game.white_elo_delta),
+      game.player_white,
+      game.result,
+      game.player_black,
+      round(game.black_elo),
+      prefix(game.black_elo_delta),
+      game.size,
+      `${game.timertime / 60}+${game.timerinc}`,
+    ].forEach((text, i) => {
+      tds[i].textContent = text;
+    });
+
+    [[2, game.white_elo_delta], [7, game.black_elo_delta]].forEach(([i, delta]) => {
+      if (delta >= 0) {
+        tds[i].classList.add("up");
+        tds[i].classList.remove("down");
+      } else {
+        tds[i].classList.add("down");
+        tds[i].classList.remove("up");
+      }
+    });
+
+    return clone;
+  }
+
+  function updateGamesTable(games) {
+    const template = getElement("template#game-row");
+    const tbody = getElement("#games tbody");
+    const gameRows = games.map((game) => createGameRow(template, game));
+    tbody.innerHTML = "";
+    gameRows.forEach((row) => tbody.appendChild(row));
+  }
+
+  console.log("update player page", data);
+  updateGamesTable(data);
+}
+
+function loadPlayer(playername) {
+  const nameElement = getElement("#playername");
+  if (!playername) {
+    nameElement.innerHTML = "No player name defined, go <a href='/'>back</a>";
+    return;
+  }
+  nameElement.textContent = playername;
+
+  const ratingRequest = window.fetch(`/api/player/${playername}`);
+  ratingRequest.then((response) => {
+    if (response.status !== 200) {
+      console.error("Failed to get player data", response);
+    }
+
+    response.json()
+      .then((json) => updatePlayerPage(json))
+      .catch((err) => console.error("Failed to parse JSON from ratings", err));
+  });
+}
+
+function genericOnLoad() {
+  console.log("Loaded");
+  getElement("#js-disabled").remove();
+
+  if (!isBrowserSupported()) {
+    console.error("This browser is not supported");
+    getElement("#browser-not-supported").classList.remove("hidden");
+  }
+}
+
+document.onload = genericOnLoad;
 document.addEventListener("DOMContentLoaded", () => {
   console.log("dom loaded");
-  onLoad();
-});
-window.addEventListener("load", () => {
-  console.log("window laoded");
+  genericOnLoad();
+
+  const urlParts = window.location.pathname.split("/");
+  if (urlParts[urlParts.length - 1] === "player.html") {
+    const params = new URLSearchParams(window.location.search);
+    const playername = params.get("name");
+    loadPlayer(playername);
+  } else {
+    loadStatistics();
+  }
 });
